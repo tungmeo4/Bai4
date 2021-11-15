6d724b8056cb5628a4409193806396fe99993d91
tungmeo4 <92002319+tungmeo4@users.noreply.github.com>
2021-11-15 15:25:06 +0700

    Initial commit

---
+++ src/Animal/Chicken.java
@@ -0,0 +1,7 @@
+package Animal;
+
+public class Chicken extends Animal{
+    @Override
+    public String makeSound() {
+        return "Chicken: Ó ó ó ó"
+}
---
+++ src/LopHinhChuNhat/main.java
@@ -0,0 +1,17 @@
+package LopHinhChuNhat;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width:");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height:");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width,height);
+        System.out.println("Your rectangle \n" + rectangle.display());
+        System.out.println("Perimeter of the LopHinhChuNhat.Rectangle:" + rectangle.getPerimeter());
+        System.out.println("Area of the LopHinhChuNhat.Rectangle:" + rectangle.getArea());
+    }
+}
